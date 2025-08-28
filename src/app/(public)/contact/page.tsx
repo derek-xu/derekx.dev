6ad3ce26087844ad25c62089376d04fe876dfe76
Derek Xu <derek@yuzu.health>
2025-08-28 18:11:34 -0400

    routing

---
+++ src/app/(public)/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="mt-6 space-y-4">
+        <p>d27xu [at] uwaterloo.ca</p>
+      </div>
+    </div>
+  );
+}
